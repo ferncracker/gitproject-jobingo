33d8b7574b1ca2b17c60784c1a3791559dcb3172
ferncracker <harsh1996_sharma@outlook.com>
2017-11-06 22:25:26 +0530

    landing page

---
+++ index.php
@@ -0,0 +1,157 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="index.css">
+<title>Jobingo</title>
+</head>
+<body>
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+<div class="container">
+<button class="navbar-toggler navbar-toggler-right" data-toggle="collapse" data-target="#navbarNav"><span class="navbar-toggler-icon"></span></button>
+
+<div class="collapse navbar-collapse" id="navbarNav">
+<a href="#" class="navbar-brand text-muted"><b>Jobingo</b></a>
+	<ul class="navbar-nav ml-auto">
+	<li class="nav-item p-2 active">
+	<a class="nav-link" href="#">HOME</a>
+	</li>
+	<li class="nav-item p-2">
+	<a class="nav-link" href="#">CONSULT</a>
+	</li>
+	<li class="nav-item p-2">
+	<a class="nav-link" href="#blog-head-section">BLOG</a>
+	</li>
+	<li class="nav-item p-2">
+	<a class="nav-link" href="#as-head-section">ABOUT US</a>
+	</li>
+	<li class="nav-item p-2">
+	<a class="nav-link" href="#as-head-section">LOGIN</a>
+	</li>
+	</ul>
+</div>
+</div>
+</nav>
+
+<!-- carousel -->
+<section id="showcase">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+<ol class="carousel-indicators">
+<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+<li data-target="#myCarousel" data-slide-to="1"></li>
+<li data-target="#myCarousel" data-slide-to="2"></li>
+	</ol>
+
+	<div class="carousel-inner">
+		<div class="carousel-item carousel-image-1 active">
+			<div class="container">
+				<div class="carousel-caption d-none d-sm-block text-right mb-5">
+					<h1 class="display-3">Heading One</h1>
+					<p class="lead">gdghhjgjhgjhgjhfhhghgjgjgjhgjk</p>
+					<a href="#" class="btn btn-danger btn-lg">Sign Up Now</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="carousel-item carousel-image-2">
+			<div class="container">
+				<div class="carousel-caption d-none d-sm-block mb-5">
+					<h1 class="display-3">Heading Two</h1>
+					<p class="lead">If already a member</p>
+					<a href="#" class="btn btn-primary btn-lg">Login Now</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="carousel-item carousel-image-3">
+			<div class="container">
+				<div class="carousel-caption d-none d-sm-block text-right mb-5">
+					<h1 class="display-3">Heading Three</h1>
+					<p class="lead">gdghhjgjhgjhgjhfhhghgjgjgjhgjk</p>
+					<a href="#" class="btn btn-success btn-lg">Create resume</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<a href="#myCarousel" data-slide="prev" class="carousel-control-prev">
+	<span class="carousel-control-prev-icon"></span>
+	</a>
+
+    <a href="#myCarousel" data-slide="next" class="carousel-control-next">
+	<span class="carousel-control-next-icon"></span>
+	</a>
+	</div>
+</section>
+
+<section id="explore-head-section" class="bg-faded">
+	<div class="container">
+		<div class="row">
+			<div class="col text-center">
+			<div class="py-4">
+				<h1 class="display-4">GET HIRED TODAY!</h1>
+				<p class="lead">List of top job sectors</p>
+				<br/>
+
+				<div class="card-columns">
+
+				<div class="card" style="width: 20rem">
+				<img class="card-img-top img-fluid" src="images/img1.jpeg" alt="">
+				<div class="card-block text-muted">
+					<h4 class="card-title">Software Developer</h4>
+					<a class="btn btn-outline-success btn-lg btn-block" href="#">Yes!</a>
+				</div>
+				</div>
+
+				<div class="card" style="width: 20rem">
+				<img class="card-img-top img-fluid" src="images/img2.jpg" alt="">
+				<div class="card-block">
+					<h4 class="card-title text-muted">Management</h4>
+					<a class="btn btn-outline-primary btn-lg btn-block" href="#">Yes!</a>
+				</div>
+				</div>
+
+				<div class="card" style="width: 20rem">
+				<img class="card-img-top img-fluid" src="images/img3.jpeg" alt="">
+				<div class="card-block">
+					<h4 class="card-title text-muted">Banking & Financial</h4>
+					<a class="btn btn-outline-danger btn-lg btn-block" href="#">GO</a>
+				</div>
+				</div>
+
+				</div>
+
+                <br/>
+				<a href="#" class="btn btn-outline-warning">Find Out More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="footer" class="text-inverse py-3">
+	<div class="container">
+	<div class="d-flex flex-row align-items-center justify-content-between">
+	<small class="small text-right"><div class="fa fa-copyright">Jobingo 2017</div></small>
+				<h1 class="align-items-right" style="font-size: 20px;">Join Us</h1>
+				<div><a href="#"><i class="fa fa-twitter fa-inverse p-2"></a></i></div>
+				<div><a href="#"><i class="fa fa-facebook fa-inverse p-2"></a></i></div>
+				<div><a href="#"><i class="fa fa-instagram fa-inverse p-2"></a></i></div>
+				<div><a href="#"><i class="fa fa-linkedin fa-inverse p-2"></a></i></div>
+            </div>
+		</div>
+</section>
+
+
+<script src="https://code.jquery-3.2.1.min.js" ></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+</body>
+</html>
